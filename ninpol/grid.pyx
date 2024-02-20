9612e33cb10ec61e1adc6b3bad1f9f60058f19c3
Davi Yan <davi.yan_ma@yahoo.com>
2024-02-20 00:24:01 -0300

    feat: Add esuel build and reorganize structure

--- ninpol/grid.pyx
+++ ninpol/grid.pyx
@@ -7,61 +7,125 @@ from libc.stdio cimport printf
 from cython.parallel cimport parallel, prange
 cimport openmp
 
+
 DTYPE_I = np.int64
 DTYPE_F = np.float64
 
 cdef class Grid:
-    def __cinit__(self, DTYPE_I_t n_dims, DTYPE_I_t n_elems, DTYPE_I_t n_points, DTYPE_I_t n_points_per_elem):
+    def __cinit__(self, DTYPE_I_t n_dims, DTYPE_I_t n_elems, DTYPE_I_t n_points):
         """
-        Initializes the grid.
-        Parameter
-        ---------
-            n_dims : int
-                Number of dimensions (i.e. 2 or 3)
-            n_elems : int
-                Number of elements in the mesh
-            n_points : int
-                Number of points (vertices) in the mesh
-            n_points_per_elem : int
-                Number of points (vertices) per element
+            Initializes the grid.
+
+            Parameters
+            ---------
+                n_dims : int
+                    Number of dimensions (i.e. 2 or 3)
+                n_elems : int
+                    Number of elements in the mesh
+                n_points : int
+                    Number of points (vertices) in the mesh
         """
+        import yaml
+        import os
+        cdef:
+            str script_dir = os.path.dirname(os.path.abspath(__file__))
+            str point_ordering_path = os.path.join(script_dir, "utils/point_ordering.yaml")
+            dict point_ordering_obj, point_ordering
+
         self.n_dims             = n_dims
         self.n_elems            = n_elems
         self.n_points           = n_points
-        self.n_points_per_elem  = n_points_per_elem
+        point_ordering_obj      = yaml.load(open(point_ordering_path), Loader=yaml.FullLoader)
+        point_ordering          = point_ordering_obj['elements']
 
+        cdef:
+            DTYPE_I_t[::1] nfael = np.zeros(16, dtype=DTYPE_I)
+            DTYPE_I_t[:, ::1] lnofa = np.zeros((16, 6), dtype=DTYPE_I)
+            DTYPE_I_t[:, :, ::1] lpofa = np.ones((16, 6, 4), dtype=DTYPE_I) * -1
+            int element_type, element_n_points
+            int i, j
+            list face
+            int point
+            str element_name
+            dict element
         
+        self.n_point_to_type = np.zeros(16, dtype=DTYPE_I)
 
-    cpdef void build(self, DTYPE_I_t[:, ::1] connectivity):
-        """
-            Builds the Elements surrounding points connectivity (esup) and the Points surrounding points connectivity (psup) from the connectivity matrix.
-            0-based indexing is assumed.
-            Assumes connectivity is contiguous in memory.
-            Parameter
-            ---------
-                connectivity : np.ndarray
-                    Connectivity matrix. Each row contains the indices of the points that form an element.
-        """
+        for element_name in point_ordering:
+            element = point_ordering[element_name]
+            element_type = element['element_type']
+            element_n_points = element['number_of_points']
+            self.n_point_to_type[element_n_points] = element_type
+
+            if element_n_points < 3:
+                continue
+
+            nfael[element_type] = len(element['faces'])
+            for i, face in enumerate(element['faces']):
+                lnofa[element_type, i] = len(face)
+                for j, point in enumerate(face):
+                    lpofa[element_type, i, j] = point
+                
+                if len(face) < 4:
+                    lpofa[element_type, i, 3] = -1
+        
+        self.nfael = nfael
+        self.lnofa = lnofa
+        self.lpofa = lpofa
+
+
+    cpdef void build(self, DTYPE_I_t[:, ::1] connectivity, DTYPE_I_t[::1] element_types = None):
+
         # Check that the connectivity matrix is not None and has the correct shape
         if connectivity is None:
             raise ValueError("The connectivity matrix cannot be None.")
         if connectivity.shape[0] != self.n_elems:
-            raise ValueError("The number of rows in the connectivity matrix must be equal to the number of elements.")
-        if connectivity.shape[1] != self.n_points_per_elem:
-            raise ValueError("The number of columns in the connectivity matrix must be equal to the number of points per element.")
 
+            raise ValueError("The number of rows in the connectivity matrix must be equal to the number of elements.")
+        
+        self.inpoel = connectivity.copy()
+        
+        # Calculate number of points per element, defines number of faces per element
         cdef:
             int i, j
-            int tid
-            
+
+        self.n_points_per_elem = np.zeros(self.n_elems, dtype=DTYPE_I)
+        self.element_types = element_types if element_types is not None else np.zeros(self.n_elems, dtype=DTYPE_I)
+
+        for i in range(self.n_elems):
+            for j in range(self.inpoel.shape[1]):
+                if self.inpoel[i, j] == -1:
+                    break
+                self.n_points_per_elem[i] += 1
+            if element_types is None:
+                self.element_types[i] = self.n_point_to_type[self.n_points_per_elem[i]]
+        
+        # Calculate the elements surrounding each point
+        self.build_esup()
+
+        # Calculate the points surrounding each point
+        self.build_psup()
+
+        # Calculate the elements surrounding each element
+        # self.build_esuel()
+
+        # Calculate the points that form each edge
+        if self.n_dims == 3:
+            self.build_inpoed()
+            self.build_ledel()
+
+    cdef void build_esup(self):
+        
+        cdef:
+            int i, j
+
         # Reshape the arrays
         self.esup_ptr = np.zeros(self.n_points+1, dtype=DTYPE_I)
-        self.psup_ptr = np.zeros(self.n_points+1, dtype=DTYPE_I)
 
         # Count the number of elements surrounding each point    
         for i in range(self.n_elems):
-            for j in range(self.n_points_per_elem):
-                self.esup_ptr[connectivity[i, j] + 1] += 1
+            for j in range(self.n_points_per_elem[i]):
+                self.esup_ptr[self.inpoel[i, j] + 1] += 1
         
         # Compute the cumulative sum of the number of elements surrounding each point
         for i in range(self.n_points):
@@ -70,37 +134,118 @@ cdef class Grid:
         # Fill the esup array
         self.esup = np.zeros(self.esup_ptr[self.n_points], dtype=DTYPE_I)
         for i in range(self.n_elems):
-            for j in range(self.n_points_per_elem):
-                self.esup[self.esup_ptr[connectivity[i, j]]] = i
-                self.esup_ptr[connectivity[i, j]] += 1
+            for j in range(self.n_points_per_elem[i]):
+                self.esup[self.esup_ptr[self.inpoel[i, j]]] = i
+                self.esup_ptr[self.inpoel[i, j]] += 1
         
         for i in range(self.n_points, 0, -1):
             self.esup_ptr[i] = self.esup_ptr[i-1]
         self.esup_ptr[0] = 0
 
+    cdef void build_psup(self):
+
         cdef:
+            int i, j, k
             int stor_ptr = 0, point_idx
             DTYPE_I_t[::1] temp_psup = np.ones(self.n_points, dtype=DTYPE_I) * -1
+        
+        self.psup_ptr = np.zeros(self.n_points+1, dtype=DTYPE_I)
         self.psup_ptr[0] = 0
     
-        # Upper bound for the number of points surrounding each point
-        self.psup = np.zeros((self.esup_ptr[self.n_points] * (self.n_points_per_elem - 1)), dtype=DTYPE_I) # Peharps this can be improved
+        # Upper bound for the number of points surrounding each point, pehaps this can be improved
+        self.psup = np.zeros((self.esup_ptr[self.n_points] * (7)), dtype=DTYPE_I) 
 
         # Calculate the points surrounding each point, using temp_psup to avoid duplicates
         for i in range(self.n_points):
             for j in range(self.esup_ptr[i], self.esup_ptr[i+1]):
-                for k in range(self.n_points_per_elem):
-                    point_idx = connectivity[self.esup[j], k]
+                for k in range(self.n_points_per_elem[self.esup[j]]):
+                    point_idx = self.inpoel[self.esup[j], k]
                     if point_idx != i and temp_psup[point_idx] != i:
                         self.psup[stor_ptr] = point_idx
                         temp_psup[point_idx] = i
                         stor_ptr += 1
                         
             self.psup_ptr[i+1] = stor_ptr
+
+        # Resize the psup array to remove padding
         self.psup = self.psup[:stor_ptr]
 
+    cdef void build_esuel(self):
 
+        # Declare every variable
+        cdef:
+            int j, k, l, m
+            int ielem, jelem
+            int ielem_type, jelem_type
+            DTYPE_I_t[::1] ielem_face = np.zeros(4, dtype=DTYPE_I)
+            DTYPE_I_t[::1] jelem_face = np.zeros(4, dtype=DTYPE_I)
+            int point, kpoint
+            int num_elems, num_elems_min
+            int found_elem
 
+        self.esuel = np.ones((self.n_elems, 6), dtype=DTYPE_I) * -1
+        # For each element
+        for ielem in range(self.n_elems):
+            ielem_type = self.element_types[ielem]
+
+            # For each face
+            for j in range(self.nfael[ielem_type]):
+                # Choose a point from the face
+                ielem_face = self.lpofa[ielem_type, j].copy()
+                for k in range(self.lnofa[ielem_type, j]):
+                    ielem_face[k] = self.inpoel[ielem, ielem_face[k]]
+
+                point = ielem_face[0]
+                num_elems_min = self.esup_ptr[point+1] - self.esup_ptr[point]
+
+                # Choose the point with the least number of elements around it
+                for k in range(self.lnofa[ielem_type, j]):
+                    kpoint = ielem_face[k]
+                    num_elems = self.esup_ptr[kpoint+1] - self.esup_ptr[kpoint]
+                    if num_elems < num_elems_min:
+                        point = kpoint
+                        num_elems_min = num_elems
+
+                ielem_face = np.sort(ielem_face)
+                found_elem = False
+
+                # For each element around the point
+                for k in range(self.esup_ptr[point], self.esup_ptr[point+1]):
+                    jelem = self.esup[k]
+                    jelem_type = self.element_types[jelem]
+
+                    # If the element around the point is not the current element
+                    if jelem != ielem:
+
+                        # For each face of the element around the point
+                        for l in range(self.nfael[jelem_type]):
+                            
+                            jelem_face = self.lpofa[jelem_type, l].copy()
+
+                            for m in range(self.lnofa[jelem_type, l]):
+                                jelem_face[m] = self.inpoel[jelem, jelem_face[m]]
+                                
+                            jelem_face = np.sort(jelem_face)
+
+                            # If the face of the element around the point is equal to the face of the current element
+                            if np.array_equal(ielem_face, jelem_face):
+
+                                # Add the element around the point to the list of elements around the current element
+                                self.esuel[ielem, j] = jelem
+                                # Add the current element to the list of elements around the element around the point
+                                self.esuel[jelem, l] = ielem
+
+                                found_elem = True
+                                break
+
+                        if found_elem:
+                            break
+                
+               
+    cdef void build_inpoed(self):
+        pass
+    cdef void build_ledel(self):
+        pass
     
 
         
--- tests/grid_np.py
+++ tests/grid_np.py
@@ -1,52 +1,178 @@
-"""
-This is a python/numpy mirror of "grid.pyx" file, used for performance and correctness testing.
-"""
-
 import numpy as np
 import numba as nb
+import yaml
 
-@nb.njit()
-def build(connectivity, n_elems, n_points_per_elem, n_points):
 
+class Grid:
+    def __init__(self, n_dims, n_elems, n_points):
+        self.n_dims         = n_dims
+        self.n_elems        = n_elems
+        self.n_points       = n_points
+        point_ordering_obj  = yaml.safe_load(open("./ninpol/utils/point_ordering.yaml"))
+        point_ordering = point_ordering_obj['elements']
+
+        nfael = np.zeros(16, dtype=np.int32)
+        lnofa = np.zeros((16, 6), dtype=np.int32)
+        lpofa = np.ones((16, 6, 4), dtype=np.int32) * -1
+
+        self.n_point_to_type = np.zeros(16, dtype=np.int32)
+
+        for element_name in point_ordering:
+            element = point_ordering[element_name]
+            element_type = element['element_type']
+            element_n_points = element['number_of_points']
+            self.n_point_to_type[element_n_points] = element_type
+
+            if element_n_points < 3:
+                continue
+
+            nfael[element_type] = len(element['faces'])
+            for i, face in enumerate(element['faces']):
+                lnofa[element_type, i] = len(face)
+                for j, point in enumerate(face):
+                    lpofa[element_type, i, j] = point
+                
+                if len(face) < 4:
+                    lpofa[element_type, i, 3] = -1
+        
+        self.nfael = nfael
+        self.lnofa = lnofa
+        self.lpofa = lpofa
+
+    def build(self, connectivity, elem_types = None):
+        return_values = build(self.n_dims, self.n_points, self.n_elems, self.n_point_to_type, self.nfael, self.lnofa, self.lpofa, connectivity, elem_types)
+        self.inpoel, self.n_points_per_elem, self.element_types, self.esup, self.esup_ptr, self.psup, self.psup_ptr, self.esuel = return_values
+
+def build(n_dims, n_points, n_elems, n_point_to_type, nfael, lnofa, lpofa, connectivity, elem_types = None):
     # Check that the connectivity matrix is not None and has the correct shape
     if connectivity is None:
         raise ValueError("The connectivity matrix cannot be None.")
     if connectivity.shape[0] != n_elems:
         raise ValueError("The number of rows in the connectivity matrix must be equal to the number of elements.")
-    if connectivity.shape[1] != n_points_per_elem:
-        raise ValueError("The number of columns in the connectivity matrix must be equal to the number of points per element.")
     
+    inpoel = connectivity
     
-    esup_ptr = np.bincount(connectivity.ravel() + 1, minlength=n_points + 1)
-    esup_ptr = np.cumsum(esup_ptr)
-    esup = np.zeros(esup_ptr[n_points], dtype=np.int32)
+    n_points_per_elem = np.array([len(np.argwhere(inpoel[i] != -1)) for i in range(n_elems)], dtype=np.int32)
+    element_types = elem_types if elem_types is not None else n_point_to_type[n_points_per_elem]
 
+    esup, esup_ptr = build_esup(n_elems, n_points, inpoel, n_points_per_elem)
+    psup, psup_ptr = build_psup(n_points, inpoel, esup, esup_ptr)
+    #esuel = build_esuel(n_elems, inpoel, element_types, nfael, lnofa, lpofa, esup, esup_ptr)
+    esuel = np.zeros((n_elems, 6), dtype=np.int32)
+    if n_dims == 3:
+        build_inpoed()
+        build_ledel()
+
+    return inpoel, n_points_per_elem, element_types, esup, esup_ptr, psup, psup_ptr, esuel
+
+@nb.njit()
+def build_esup(n_elems, n_points, inpoel, n_points_per_elem):
+    esup_ptr = np.zeros(n_points + 1, dtype=np.int32)
     for i in range(n_elems):
-        for j in range(n_points_per_elem):
-            esup[esup_ptr[connectivity[i, j]]] = i
-            esup_ptr[connectivity[i, j]] += 1
-        
+        for j in range(n_points_per_elem[i]):
+            esup_ptr[inpoel[i, j] + 1] += 1
+    esup_ptr = np.cumsum(esup_ptr)  
+    esup = np.zeros(esup_ptr[n_points], dtype=np.int32)
+    for i in range(n_elems):
+        for j in range(n_points_per_elem[i]):
+            esup[esup_ptr[inpoel[i, j]]] = i
+            esup_ptr[inpoel[i, j]] += 1
     for i in range(n_points, 0, -1):
         esup_ptr[i] = esup_ptr[i-1]
 
     esup_ptr[0] = 0
+    return esup, esup_ptr
 
+@nb.njit()
+def build_psup(n_points, inpoel, esup, esup_ptr):
     psup_ptr = np.zeros(n_points + 1, dtype=np.int32)
-    psup = np.zeros(esup_ptr[n_points] * (n_points_per_elem - 1), dtype=np.int32)
+    psup = np.zeros(esup_ptr[n_points] * (7), dtype=np.int32)
     stor_ptr = 0
+
     for i in range(n_points):
         elems = esup[esup_ptr[i]:esup_ptr[i+1]]
         if elems.shape[0] == 0:
             continue
-        x = connectivity[elems, :].flatten()
+        x = inpoel[elems, :].flatten()
         mx    = np.max(x) + 1
-        used  = np.zeros(mx,dtype=np.uint8)
+        used  = np.zeros(mx, dtype=np.uint8)
         used[x] = 1
         points = np.argwhere(used == 1)[:,0]
         points = points[points != i]
         psup[stor_ptr:stor_ptr + points.shape[0]] = points
         psup_ptr[i+1] = psup_ptr[i] + points.shape[0]
         stor_ptr += points.shape[0]
+
     psup = psup[:psup_ptr[-1]]
-    return esup, esup_ptr, psup, psup_ptr
-    
+
+    return psup, psup_ptr
+
+@nb.njit()
+def build_esuel(n_elems, inpoel, element_types, nfael, lnofa, lpofa, esup, esup_ptr):
+    esuel = np.ones((n_elems, 6), dtype=np.int32) * -1
+
+    # For each element
+    for ielem in range(n_elems):
+        ielem_type = element_types[ielem]
+
+        # For each face
+        for j in range(nfael[ielem_type]):
+            # Choose a point from the face
+            ielem_face = lpofa[ielem_type, j].copy()
+            for k in range(lnofa[ielem_type, j]):
+                ielem_face[k] = inpoel[ielem, ielem_face[k]]
+
+            point = ielem_face[0]
+            num_elems_min = esup_ptr[point+1] - esup_ptr[point]
+
+            # Choose the point with the least number of elements around it
+            for k in range(lnofa[ielem_type, j]):
+                kpoint = ielem_face[k]
+                num_elems = esup_ptr[kpoint+1] - esup_ptr[kpoint]
+                if num_elems < num_elems_min:
+                    point = kpoint
+                    num_elems_min = num_elems
+
+            ielem_face = np.sort(ielem_face)
+            found_elem = False
+
+            # For each element around the point
+            for k in range(esup_ptr[point], esup_ptr[point+1]):
+                jelem = esup[k]
+                jelem_type = element_types[jelem]
+
+                # If the element around the point is not the current element
+                if jelem != ielem:
+
+                    # For each face of the element around the point
+                    for l in range(nfael[jelem_type]):
+                        
+                        jelem_face = lpofa[jelem_type, l].copy()
+                        for m in range(lnofa[jelem_type, l]):
+                            jelem_face[m] = inpoel[jelem, jelem_face[m]]
+                            
+                        jelem_face = np.sort(jelem_face)
+
+                        # If the face of the element around the point is equal to the face of the current element
+                        if np.array_equal(ielem_face, jelem_face):
+
+                            # Add the element around the point to the list of elements around the current element
+                            esuel[ielem, j] = jelem
+                            # Add the current element to the list of elements around the element around the point
+                            esuel[jelem, l] = ielem
+
+                            found_elem = True
+                            break
+
+                        if found_elem:
+                            break
+                
+    return esuel
+
+@nb.njit()
+def build_inpoed():
+    pass
+
+@nb.njit()
+def build_ledel():
+    pass
